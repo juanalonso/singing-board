0afb8bd17651114d399ffd5f3220ff6fcb2b2ca9
Juan Alonso <juan@neuronasmuertas.com>
2020-05-24 01:59:26 +0200

    Adapted to pulse train

--- hall_sensor/hall_sensor.ino
+++ hall_sensor/hall_sensor.ino
@@ -5,76 +5,100 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioPlayMemory          impulse;        //xy=144,95
-AudioMixer4              mix_delay_impulse; //xy=345,115
-AudioEffectDelay         delay_impulse;  //xy=345,315
-AudioFilterBiquad        biquad1;        //xy=588,115
-AudioFilterBiquad        biquad2;        //xy=588,175
-AudioFilterBiquad        biquad3;        //xy=588,235
-AudioFilterBiquad        biquad4;        //xy=588,295
-AudioMixer4              mix_filter;     //xy=785,134
-AudioEffectFreeverb      reverb;         //xy=936,232
-AudioMixer4              mix_reverb;     //xy=1092,153
-AudioOutputI2S           i2s;            //xy=1289,146
-AudioConnection          patchCord1(impulse, 0, mix_delay_impulse, 0);
-AudioConnection          patchCord2(mix_delay_impulse, delay_impulse);
-AudioConnection          patchCord3(mix_delay_impulse, biquad1);
-AudioConnection          patchCord4(mix_delay_impulse, biquad2);
-AudioConnection          patchCord5(mix_delay_impulse, biquad3);
-AudioConnection          patchCord6(mix_delay_impulse, biquad4);
-AudioConnection          patchCord7(delay_impulse, 0, mix_delay_impulse, 1);
-AudioConnection          patchCord8(biquad1, 0, mix_filter, 0);
-AudioConnection          patchCord9(biquad2, 0, mix_filter, 1);
-AudioConnection          patchCord10(biquad3, 0, mix_filter, 2);
-AudioConnection          patchCord11(biquad4, 0, mix_filter, 3);
-AudioConnection          patchCord12(mix_filter, 0, mix_reverb, 0);
-AudioConnection          patchCord13(mix_filter, reverb);
-AudioConnection          patchCord14(reverb, 0, mix_reverb, 1);
-AudioConnection          patchCord15(mix_reverb, 0, i2s, 0);
-AudioConnection          patchCord16(mix_reverb, 0, i2s, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=1278,212
+AudioSynthPulsetrain     impulse;        //xy=286,98
+AudioFilterBiquad        biquad1;        //xy=565,98
+AudioFilterBiquad        biquad2;        //xy=565,158
+AudioFilterBiquad        biquad3;        //xy=565,218
+AudioFilterBiquad        biquad4;        //xy=565,278
+AudioMixer4              mix_filter;     //xy=762,117
+AudioEffectFreeverb      reverb;         //xy=913,215
+AudioMixer4              mix_reverb;     //xy=1069,136
+AudioOutputI2S           i2s;            //xy=1266,129
+AudioConnection          patchCord1(impulse, biquad1);
+AudioConnection          patchCord2(impulse, biquad2);
+AudioConnection          patchCord3(impulse, biquad3);
+AudioConnection          patchCord4(impulse, biquad4);
+AudioConnection          patchCord5(biquad1, 0, mix_filter, 0);
+AudioConnection          patchCord6(biquad2, 0, mix_filter, 1);
+AudioConnection          patchCord7(biquad3, 0, mix_filter, 2);
+AudioConnection          patchCord8(biquad4, 0, mix_filter, 3);
+AudioConnection          patchCord9(mix_filter, 0, mix_reverb, 0);
+AudioConnection          patchCord10(mix_filter, reverb);
+AudioConnection          patchCord11(reverb, 0, mix_reverb, 1);
+AudioConnection          patchCord12(mix_reverb, 0, i2s, 0);
+AudioConnection          patchCord13(mix_reverb, 0, i2s, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=1255,195
 // GUItool: end automatically generated code
 
 
 
-#include <Bounce.h>
-#include "AudioSampleImpulse_x2.h"
-//#include "Coefficcients.h"
 
+#include <Bounce.h>
+
+
+//OLIVIA
 double Coefficcients[20][5] = {
   //A
-  {1.0, 0.0, 0.0, -1.9842, 0.9915},
-  {1.0, 0.0, 0.0, -1.9682, 0.9901},
-  {1.0, 0.0, 0.0, -1.8833, 0.9844},
-  {1.0, 0.0, 0.0, -1.8634, 0.9830},
+  {1.0, 0.0, 0.0, -1.9619, 0.9915},
+  {1.0, 0.0, 0.0, -1.9204, 0.9901},
+  {1.0, 0.0, 0.0, -1.7174, 0.9844},
+  {1.0, 0.0, 0.0, -1.3346, 0.9830},
   //E
-  {1.0, 0.0, 0.0, -1.9911, 0.9943},
-  {1.0, 0.0, 0.0, -1.9359, 0.9887},
-  {1.0, 0.0, 0.0, -1.8708, 0.9859},
-  {1.0, 0.0, 0.0, -1.8273, 0.9830},
+  {1.0, 0.0, 0.0, -1.9847, 0.9943},
+  {1.0, 0.0, 0.0, -1.8579, 0.9887},
+  {1.0, 0.0, 0.0, -1.7480, 0.9859},
+  {1.0, 0.0, 0.0, -1.3789, 0.9830},
   //I
-  {1.0, 0.0, 0.0, -1.9902, 0.9915},
-  {1.0, 0.0, 0.0, -1.9258, 0.9873},
-  {1.0, 0.0, 0.0, -1.8511, 0.9859},
-  {1.0, 0.0, 0.0, -1.7987, 0.9830},
+  {1.0, 0.0, 0.0, -1.9912, 0.9943},
+  {1.0, 0.0, 0.0, -1.8175, 0.9887},
+  {1.0, 0.0, 0.0, -1.7318, 0.9859},
+  {1.0, 0.0, 0.0, -1.4438, 0.9830},
   //O
-  {1.0, 0.0, 0.0, -1.9911, 0.9943},
-  {1.0, 0.0, 0.0, -1.9773, 0.9887},
-  {1.0, 0.0, 0.0, -1.8708, 0.9859},
-  {1.0, 0.0, 0.0, -1.8485, 0.9830},
+  {1.0, 0.0, 0.0, -1.9854, 0.9943},
+  {1.0, 0.0, 0.0, -1.9555, 0.9887},
+  {1.0, 0.0, 0.0, -1.6965, 0.9859},
+  {1.0, 0.0, 0.0, -1.4362, 0.9830},
   //U
-  {1.0, 0.0, 0.0, -1.9918, 0.9943},
-  {1.0, 0.0, 0.0, -1.9814, 0.9887},
-  {1.0, 0.0, 0.0, -1.8708, 0.9859},
-  {1.0, 0.0, 0.0, -1.8407, 0.9830},
+  {1.0, 0.0, 0.0, -1.9922, 0.9943},
+  {1.0, 0.0, 0.0, -1.9682, 0.9887},
+  {1.0, 0.0, 0.0, -1.7448, 0.9859},
+  {1.0, 0.0, 0.0, -1.4172, 0.9830},
 };
 
+
+//RANDOM GUY
+//double Coefficcients[20][5] = {
+//  //A
+//  {1.0, 0.0, 0.0, -1.9842, 0.9915},
+//  {1.0, 0.0, 0.0, -1.9682, 0.9901},
+//  {1.0, 0.0, 0.0, -1.8833, 0.9844},
+//  {1.0, 0.0, 0.0, -1.8634, 0.9830},
+//  //E
+//  {1.0, 0.0, 0.0, -1.9911, 0.9943},
+//  {1.0, 0.0, 0.0, -1.9359, 0.9887},
+//  {1.0, 0.0, 0.0, -1.8708, 0.9859},
+//  {1.0, 0.0, 0.0, -1.8273, 0.9830},
+//  //I
+//  {1.0, 0.0, 0.0, -1.9902, 0.9915},
+//  {1.0, 0.0, 0.0, -1.9258, 0.9873},
+//  {1.0, 0.0, 0.0, -1.8511, 0.9859},
+//  {1.0, 0.0, 0.0, -1.7987, 0.9830},
+//  //O
+//  {1.0, 0.0, 0.0, -1.9911, 0.9943},
+//  {1.0, 0.0, 0.0, -1.9773, 0.9887},
+//  {1.0, 0.0, 0.0, -1.8708, 0.9859},
+//  {1.0, 0.0, 0.0, -1.8485, 0.9830},
+//  //U
+//  {1.0, 0.0, 0.0, -1.9918, 0.9943},
+//  {1.0, 0.0, 0.0, -1.9814, 0.9887},
+//  {1.0, 0.0, 0.0, -1.8708, 0.9859},
+//  {1.0, 0.0, 0.0, -1.8407, 0.9830},
+//};
 #define NUMNOTES   7
-#define MIDIOFFSET 60
+#define MIDIOFFSET 58
 
 int digitalPins[] = {0, 1, 2, 3, 4, 5, 9, 14, 16, 17, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33};
-//float notes[NUMNOTES] = {0, 2, 4, 5, 7, 9, 11};
-float notes[NUMNOTES] = {20.75, 14.6, 9.6, 5.3, 3.4, -0.38, -3.76};
+float notes[NUMNOTES] = {0, 2, 3, 5, 7, 8, 10};
 
 #define NOTE1_PIN   digitalPins[0]
 #define NOTE2_PIN   digitalPins[1]
@@ -114,20 +138,19 @@ void setup() {
 
 
   //Teensy audio init
-  AudioMemory(160);
+  AudioMemory(80);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.85);
-  mix_delay_impulse.gain(0, 0.025);
-  mix_delay_impulse.gain(1, 0);
+  sgtl5000_1.volume(0.75);
   setCoeffs(currentVowel);
-  mix_filter.gain(0, 0.1);
-  mix_filter.gain(1, 0.1);
-  mix_filter.gain(2, 0.1);
-  mix_filter.gain(3, 0.1);
+  mix_filter.gain(0, 0.5);
+  mix_filter.gain(1, 0.5);
+  mix_filter.gain(2, 0.5);
+  mix_filter.gain(3, 0.5);
   mix_reverb.gain(0, 0.9); // 90% dry
   mix_reverb.gain(1, 0.1); // 10% wet
   reverb.roomsize(0.6);
-  reverb.damping(0.2);
+  reverb.damping(0.4);
+
 
   //Hardware init
   for (int f = 0; f < NUMNOTES; f++) {
@@ -149,21 +172,14 @@ void loop() {
     butNote[f].update();
 
     if (butNote[f].risingEdge()) {
-      mix_delay_impulse.gain(1, 0.7);
+      impulse.amplitude(0);
       digitalWrite(LED_PIN, LOW);
     }
 
     if (butNote[f].fallingEdge()) {
-
-//      Serial.print("f=");
-//      Serial.print(f);
-//      Serial.print("     notes[f]=");
-//      Serial.println(MIDIOFFSET + notes[f]);
-      float period = butToPeriod(MIDIOFFSET + notes[f]);
-      delay_impulse.delay(0, period);
-      mix_delay_impulse.gain(1, 0.99);
-      impulse.play(AudioSampleImpulse_x2);
-
+      float freq = midiToFreq(MIDIOFFSET + notes[f]);
+      impulse.frequency(freq);
+      impulse.amplitude(1);
       digitalWrite(LED_PIN, HIGH);
       break;
     }
@@ -201,8 +217,7 @@ void loop() {
 }
 
 
-
-float butToPeriod(float midiVal) {
+float midiToFreq(float midiVal) {
 
   float hz = 440.0 * pow(2, (midiVal - 69.0) / 12.0);
   float period = 1000.0 / hz;
@@ -214,22 +229,8 @@ float butToPeriod(float midiVal) {
   Serial.print("hz    Period: ");
   Serial.print(period);
   Serial.println("ms");
-  return period;
+  return hz;
 }
-
-//
-//float potToPeriod(int potValue) {
-//  int midi = map(potValue, 0, 1023, 0, 24);
-//  float hz = 110.0 * pow(1.059463094359295, midi);
-//  float period = 10000.0 / (float)hz;
-//  Serial.print("Period: ");
-//  Serial.print(hz);
-//  Serial.print("Hz / ");
-//  Serial.print(period);
-//  Serial.println("ms");
-//  return period;
-//}
-
 
 
 void setCoeffs(int v) {
